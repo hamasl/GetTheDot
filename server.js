d915767781cd539ccf339b9f435e6f2457cf20c6
Martin Sind Hagen <60466242+MartinSlindHagen@users.noreply.github.com>
2021-03-01 18:08:42 +0100

    Added gitignore

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express()
 app.use(express.static(__dirname))
 const PORT = '8080'
-/*app.use((req, res) => {
-  console.log(req.headers);
-})*/
 
 app.get("/", (req, res) => {
   res.sendFile("html/index.html", { root: __dirname })
