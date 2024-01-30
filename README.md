29c0961b7a1551f4ecefa015aa15c8b7758df689
Martin Hagen <martin@partins.no>
2024-01-30 19:47:05 +0100

    Tried running the game after many years for fun, and saw that there were some minor errors in getting it to run. Fixed those, updated the README accordingly, along some minor changes in server.js

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # GetTheDot
+
 Project i made for fun and to learn JS. Completed it during 3 days in the summer of 2020.
 
 ## Requirements:
-node  
-npm install express
+
+- node
+- expressjs
 
 ## Run by using:
-node server.js  
-Open web browser on localhost:8080
+
+```
+npm install
+npm start
+```
+
+Open web browser on localhost:8080, then press start game (easy is default difficulty), it is recommended to move around with wasd (although arrows can be used), to change color of the player press c.
 
 ## Future:
+
 Might add more functinality later which is found in the future functionality folder.
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const app = express()
-app.use(express.static(__dirname))
-const PORT = '8080'
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname));
+const PORT = "8080";
 
-app.get("/", (req, res) => {
-  res.sendFile("html/index.html", { root: __dirname })
-})
+app.get("/", (_, res) => {
+  res.sendFile("html/index.html", { root: __dirname });
+});
 
-app.listen(PORT, () => console.log("Server started running started running"))
+app.listen(PORT, () =>
+  console.log(`Server started running started running on localhost:${PORT}`)
+);
